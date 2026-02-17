54907859cc96c161b4d5caa1bd9ea20d284e61b6
ofer-blip <116800665+ofer-blip@users.noreply.github.com>
2026-02-17 12:48:39 +0200

    Clarify comment about critical map display fix
    Updated comment to clarify critical fix for map display.

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -94,7 +94,7 @@ const MapComponent: React.FC<MapComponentProps> = ({ schools, onMarkerClick, sel
     }
   }, [schools, selectedId, onMarkerClick]);
 
-  // הגדרת סטייל ישיר להבטחת תצוגה
+  // הגדרת סטייל ישיר להבטחת תצוגה - זה התיקון הקריטי למפה הלבנה
   return <div ref={mapContainerRef} style={{ height: '100%', minHeight: '500px', width: '100%' }} />;
 };
 
